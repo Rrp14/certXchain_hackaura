3a3c2fd0ea53e545a419d8c5d6d56db637c453c2
Rahul <rahulrp3031@gmail.com>
2025-10-05 10:06:07 +0530

    Clear Firebase config values
    Removed sensitive Firebase configuration details.

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -8,13 +8,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB2YEvf14X2b5oZztcCy0pChpdInbeugpE",
-  authDomain: "certification-validator.firebaseapp.com",
-  projectId: "certification-validator",
-  storageBucket: "certification-validator.firebasestorage.app",
-  messagingSenderId: "682375023325",
-  appId: "1:682375023325:web:a08ad7a7f5ab04f39c2934",
-  measurementId: "G-34LVWSNSZ7"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
